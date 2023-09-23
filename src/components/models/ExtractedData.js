7d349dccdd8aa74578dd63bf7040681075cdd31b
ryantzr1 <dreisprung1821@gmail.com>
2023-09-23 19:43:19 +0800

    Add file handling and backend capabilities
    We can now store the parsed JSON in a MongoDB object.
    
    Added backend APIs to save JSON object and to parseInvoice object.

---
+++ src/components/models/ExtractedData.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const DataSchema = new Schema({
+  data: Schema.Types.Mixed, // This allows us to store any JSON object
+  fileName: String,
+  createdOn: Date,
+  fileType: {
+    type: String,
+    enum: ["PDF", "PPT", "XLS", "JPG"],
+  },
+});
+
+const ExtractedData =
+  (mongoose.models ? mongoose.models.ExtractedData : null) ||
+  mongoose.model("ExtractedData", DataSchema);
+
+module.exports = ExtractedData;

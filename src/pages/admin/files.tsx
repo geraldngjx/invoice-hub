1676abc6f66950f8a6c3841aaebec1bb41907951
geraldngjx <geraldngjx@gmail.com>
2023-09-23 17:18:38 +0800

    Add feature to download CSV file

--- src/pages/admin/files.tsx
+++ src/pages/admin/files.tsx
@@ -1,5 +1,22 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { FilesContent } from "../../components/files/FilesContent";
 
 export default function FilesPage() {
-  return <FilesContent title="Files" />;
+  const [files, setFiles] = useState([]);
+
+  // Edit this function to query mongodb for files
+  useEffect(() => {
+    // Replace 'your-mongodb-api-url' with the actual MongoDB API URL
+    axios.get("your-mongodb-api-url")
+      .then((response: any) => {
+        // Assuming the API response contains the files data as an array
+        setFiles(response.data);
+      })
+      .catch((error: any) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return <FilesContent title="Files" files={files} />;
 }
